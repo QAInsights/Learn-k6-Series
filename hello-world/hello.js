61ac738601058cf5cb832bdc2e7f29c7ccf0df07
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-05-08 11:38:29 -0400

    Enhanced the Options

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,10 +1,16 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+// init
+export let options = {
+  vus: 5,
+  duration: '60s',
+};
 
+// vu script
 export default function () {
   let res = http.get('http://test.k6.io');
   console.log(res.status);
   
-  sleep(1);
+  sleep(5);
 }
--- online-boutique/online-boutique.js
+++ online-boutique/online-boutique.js
@@ -3,6 +3,18 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 import { check } from 'k6';
 
+export let options = {
+  stages: [
+    { duration: '5s', target: 1 },
+    { duration: '10s', target: 3 },
+    { duration: '5s', target: 1 },
+  ],
+  thresholds: {
+    // 99% of requests must finish within 1000ms.
+    http_req_duration: ['p(99) < 1000'],
+  },
+};
+
 
 export default function () {
   let homepage = http.get('https://onlineboutique.dev');
