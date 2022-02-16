a93cfc944e2649f5b3a6aa7c613e3190e4364287
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2022-02-15 23:19:19 -0500

    Add: Outputs

--- metrics-outputs/metrics-outputs-demo.js
+++ metrics-outputs/metrics-outputs-demo.js
@@ -10,48 +10,55 @@ import {
 } from "k6/metrics";
 import http from "k6/http";
 
-const customTrend = new Trend("trend_duration");
-const customCounter = new Counter("counter");
-const customGauge = new Gauge("gauge");
-const customRate = new Rate("rate");
+const customTrend = new Trend("custom_duration");
+const customCounter = new Counter("custom_counter");
+const customGauge = new Gauge("custom_gauge");
+const customRate = new Rate("custom_rate");
 
-// export const options = {
-//     scenarios: {
-//         constant_scenario: {
-//             executor: "constant-vus",
-//             vus: 2,
-//             duration: "5s",
-//             startTime: '0s'
-//         },
-//     },
-// };
+export const options = {
+    ext: {
+        loadimpact: {
+            projectID: 3571382,
+            name: "Metrics Outputs Demo",
+        }
+    },
+    scenarios: {
+        constant_scenario: {
+            executor: "constant-vus",
+            vus: 1,
+            duration: "5s",
+            startTime: '0s'
+        },
+    },
+};
 
 export default function () {
     const res = http.get("https://onlineboutique.dev");
-    sleep(1);
+
     check(res, {
         'is status 200': r => r.status === 200,
     });
 
+    //Custom trend
     console.log('Response time (ms) was ' + String(res.timings.duration));
     customTrend.add(res.timings.duration);
-    console.log('Response trend ' + customTrend.name);
 
-    //Counter - sum of all values
+    // //Counter - (sum of all values * each iteration)
     customCounter.add(1);
-    customCounter.add(5);
-    customCounter.add(10);
+    customCounter.add(2);
+    customCounter.add(3);
 
-    //Rate 50% pass - 50% fail
+    // //Rate 50% pass - 50% fail
     customRate.add(1);
     customRate.add(true);
     customRate.add(false);
     customRate.add(0);
 
-    //Gauge - displays the last value, along with min and max
+    // //Gauge - displays the last value, along with min and max
     customGauge.add(1);
     customGauge.add(5);
     customGauge.add(10);
 
+    sleep(1);
 
 }
